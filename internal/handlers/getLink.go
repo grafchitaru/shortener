87edd5f2d7a70f72f63fe9fd6e634e51cb53f953
Дмитрий Кириллов <d.kirillov@webinar.ru>
2023-12-05 15:24:34 +0400

    Переписал на клиент github.com/go-chi/chi/v5

--- internal/handlers/getLink.go
+++ internal/handlers/getLink.go
@@ -1,13 +1,13 @@
 package handlers
 
 import (
+	"github.com/go-chi/chi/v5"
 	"github.com/grafchitaru/shortener/internal/storage"
 	"net/http"
-	"strings"
 )
 
 func GetLink(res http.ResponseWriter, req *http.Request, storage storage.Repositories) {
-	path := strings.TrimPrefix(req.URL.Path, "/")
+	path := chi.URLParam(req, "id")
 	alias, err := storage.GetURL(path)
 	if err != nil {
 		res.WriteHeader(http.StatusBadRequest)
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,12 +1,28 @@
 package server
 
-import "net/http"
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/grafchitaru/shortener/internal/handlers"
+	"github.com/grafchitaru/shortener/internal/storage/sqlite"
+	"net/http"
+)
 
 func Server() {
-	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, MainPage)
+	storage, err := sqlite.New("././internal/storage/storage.db")
+	if err != nil {
+		panic(err)
+	}
 
-	err := http.ListenAndServe(`:8080`, mux)
+	r := chi.NewRouter()
+	r.Get("/{id}", func(res http.ResponseWriter, req *http.Request) {
+		handlers.GetLink(res, req, storage)
+	})
+
+	r.Post("/", func(res http.ResponseWriter, req *http.Request) {
+		handlers.CreateLink(res, req, storage)
+	})
+
+	err = http.ListenAndServe(":8080", r)
 	if err != nil {
 		panic(err)
 	}
