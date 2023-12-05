cbeae1255f37b8450f59518a4b8dff21ea60c57b
Дмитрий Кириллов <d.kirillov@webinar.ru>
2023-12-05 19:25:30 +0400

    Добавил дефолтные значения

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,8 +6,8 @@ import (
 )
 
 type Config struct {
-	HTTPServerAddress string `env:"SERVER_ADDRESS"`
-	BaseShortURL      string `env:"BASE_URL"`
+	HTTPServerAddress string `env:"SERVER_ADDRESS" envDefault:"127.0.0.1:8080"`
+	BaseShortURL      string `env:"BASE_URL" envDefault:"http://127.0.0.1:8080"`
 }
 
 func NewConfig() *Config {
