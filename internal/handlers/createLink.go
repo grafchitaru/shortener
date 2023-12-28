af171228140c506b9bb50a66bfe2247d2009b5a2
Дмитрий Кириллов <d.kirillov@webinar.ru>
2023-12-28 18:09:29 +0400

    Поменял логику

--- internal/handlers/createLink.go
+++ internal/handlers/createLink.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"compress/gzip"
 	"github.com/grafchitaru/shortener/internal/app"
 	"github.com/grafchitaru/shortener/internal/config"
 	"io"
@@ -8,14 +9,29 @@ import (
 )
 
 func CreateLink(ctx config.HandlerContext, res http.ResponseWriter, req *http.Request) {
-	reqBody, err := io.ReadAll(req.Body)
-	if err != nil {
-		http.Error(res, err.Error(), http.StatusBadRequest)
+	var reader io.Reader
+
+	if req.Header.Get(`Content-Encoding`) == `gzip` {
+		gz, err := gzip.NewReader(req.Body)
+		if err != nil {
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		reader = gz
+		defer gz.Close()
+	} else {
+		reader = req.Body
+	}
+
+	body, ioError := io.ReadAll(reader)
+	if ioError != nil {
+		http.Error(res, ioError.Error(), http.StatusBadRequest)
 		return
 	}
+
 	alias := app.NewRandomString(6)
 
-	ctx.Repos.SaveURL(string(reqBody), alias)
+	ctx.Repos.SaveURL(string(body), alias)
 	res.WriteHeader(http.StatusCreated)
 	res.Write([]byte(ctx.Config.BaseShortURL + "/" + alias))
 }
