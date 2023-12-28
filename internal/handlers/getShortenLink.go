e7257e47ae0d761156851e5b4685a1c552a5874a
Дмитрий Кириллов <d.kirillov@webinar.ru>
2023-12-28 13:27:57 +0400

    Поменял логику

--- internal/handlers/getShortenLink.go
+++ internal/handlers/getShortenLink.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"bytes"
 	"encoding/json"
+	"github.com/grafchitaru/shortener/internal/app"
 	"github.com/grafchitaru/shortener/internal/config"
 	"net/http"
 )
@@ -30,10 +31,12 @@ func GetShorten(ctx config.HandlerContext, res http.ResponseWriter, req *http.Re
 	}
 	url := link.URL
 
+	status := http.StatusOK
 	alias, err := ctx.Repos.GetAlias(url)
 	if err != nil {
-		http.Error(res, err.Error(), http.StatusBadRequest)
-		return
+		alias := app.NewRandomString(6)
+		ctx.Repos.SaveURL(url, alias)
+		status = http.StatusCreated
 	}
 
 	result := Result{
@@ -46,6 +49,6 @@ func GetShorten(ctx config.HandlerContext, res http.ResponseWriter, req *http.Re
 	}
 
 	res.Header().Set("Content-Type", "application/json")
-	res.WriteHeader(http.StatusOK)
+	res.WriteHeader(status)
 	res.Write([]byte(resp))
 }
