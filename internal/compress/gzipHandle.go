fa6316d7eb6e5aeee642f6cbe14da806da0eca77
Дмитрий Кириллов <d.kirillov@webinar.ru>
2023-12-28 15:43:24 +0400

    Поменял логику

--- internal/compress/gzipHandle.go
+++ internal/compress/gzipHandle.go
@@ -17,7 +17,13 @@ func (w gzipWriter) Write(b []byte) (int, error) {
 }
 
 func GzipHandleCompress(next http.Handler) http.Handler {
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("Content-type") != "application/json" && r.Header.Get("Content-type") != "text/html" {
+			next.ServeHTTP(w, r)
+			return
+		}
+
 		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
 			gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
 			if err != nil {
