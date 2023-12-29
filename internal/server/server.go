1f329d0aa9f44512e6c9e589e1828c9af55d2d94
Дмитрий Кириллов <d.kirillov@webinar.ru>
2023-12-29 15:33:53 +0400

    Перенес инициализацию хранилища в main

--- internal/server/server.go
+++ internal/server/server.go
@@ -7,27 +7,10 @@ import (
 	"github.com/grafchitaru/shortener/internal/config"
 	"github.com/grafchitaru/shortener/internal/handlers"
 	"github.com/grafchitaru/shortener/internal/logger"
-	storage2 "github.com/grafchitaru/shortener/internal/storage"
-	"github.com/grafchitaru/shortener/internal/storage/file"
-	"github.com/grafchitaru/shortener/internal/storage/inmemory"
-	"github.com/grafchitaru/shortener/internal/storage/sqlite"
 	"net/http"
 )
 
-func New(cfg config.Config) {
-	var storage storage2.Repositories
-	var err error
-
-	if cfg.UseDatabaseFile {
-		storage, err = file.New(cfg.FileDatabasePath)
-	} else if cfg.UseSqlite {
-		storage, err = sqlite.New(cfg.SqliteStoragePath)
-	} else {
-		storage = inmemory.New()
-	}
-	if err != nil {
-		fmt.Println("Error initialize storage: %w", err)
-	}
+func New(ctx config.HandlerContext) {
 
 	r := chi.NewRouter()
 
@@ -35,18 +18,18 @@ func New(cfg config.Config) {
 	r.Use(compress.WithCompressionResponse)
 
 	r.Get("/{id}", func(res http.ResponseWriter, req *http.Request) {
-		handlers.GetLink(config.HandlerContext{Config: cfg, Repos: storage}, res, req)
+		handlers.GetLink(ctx, res, req)
 	})
 
 	r.Post("/", func(res http.ResponseWriter, req *http.Request) {
-		handlers.CreateLink(config.HandlerContext{Config: cfg, Repos: storage}, res, req)
+		handlers.CreateLink(ctx, res, req)
 	})
 
 	r.Post("/api/shorten", func(res http.ResponseWriter, req *http.Request) {
-		handlers.GetShorten(config.HandlerContext{Config: cfg, Repos: storage}, res, req)
+		handlers.GetShorten(ctx, res, req)
 	})
 
-	err = http.ListenAndServe(cfg.HTTPServerAddress, r)
+	err := http.ListenAndServe(ctx.Config.HTTPServerAddress, r)
 	if err != nil {
 		fmt.Println("Error server: %w", err)
 	}
